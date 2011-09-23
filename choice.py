0bbf48b44d46cc1d534ec7353d7c3bd17b90dae4
zmsmith <zmsmith27@gmail.com>
2011-09-23 14:17:47 -0400

    I'm an idiot

--- choice.py
+++ choice.py
@@ -1,7 +1,12 @@
 import random
 import subprocess
 import sys
+from optparse import OptionParser
 
-def choose(choices):
-    day_of_week = random.choice(choices)
+def choose():    
+    parser = OptionParser()
+    opts, args = parser.parse_args()
+    
+    
+    day_of_week = random.choice(args)
     subprocess.call("say %s" % day_of_week, shell=True)
